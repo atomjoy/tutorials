d79b33ee5456e6bb007b404c97beec1babd6a6ee
Atomjoy <atomjoy.official@gmail.com>
2022-11-29 15:48:10 +0100

    Dynamiczna zmiana tłumaczenia title strony w vue3

---
+++ vue/components/PageTitleSetup.vue
@@ -0,0 +1,34 @@
+<!-- 
+  First add in i18n lang.js config param: 
+  allowComposition: true 
+ -->
+ 
+<template></template>
+<script setup>
+import { watch, onMounted } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t, locale } = useI18n({ useScope: 'global' })
+
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'error404.title', // Translation key from lang_en.json translation messages
+  },
+})
+
+const title = props.title
+
+onMounted(() => {
+  document.title = t(title)
+  // console.log('Title', title, locale.value)
+})
+
+watch(
+  () => locale.value,
+  (lang) => {
+    document.title = t(title)
+    // console.log('Title, locale', t(title), lang)
+  }
+)
+</script>
