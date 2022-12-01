c33feebc4bbd1316debbcb32b2b0c88bc7ebd9cf
Atomjoy <atomjoy.official@gmail.com>
2022-12-01 08:03:09 +0100

    Alert component

--- vue/components/Alert.vue
+++ vue/components/Alert.vue
@@ -17,59 +17,48 @@ const props = defineProps({
 </script>
 
 <template>
-	<div ref="div" class="animate__animated animate__flipInX" :class="'alert-' + type">{{ msg }}</div>
+	<div ref="div" class="animate__animated animate__flipInX" :class="'alert-default alert-' + type">{{ msg }}</div>
 </template>
 
 <style lang="css" scoped>
-.alert-info {
+.alert-default {
 	font-size: 0.8rem;
 	display: block;
 	padding: 10px 20px;
 	margin: 10px 0px;
+	border-radius: 10px;
+	font-weight: 700;
+}
+.alert-info {
 	color: #6f6af8;
 	background: #dddcfa;
 	border: 1px solid #6f6af8;
-	border-radius: var(--radius);
 }
 .alert-success {
-	font-size: 0.8rem;
-	display: block;
-	padding: 10px 20px;
-	margin: 10px 0px;
 	color: #5c5;
 	background: #dffbdb;
 	border: 1px solid #5c5;
-	border-radius: var(--radius);
 }
 .alert-warning {
-	font-size: 0.8rem;
-	display: block;
-	padding: 10px 20px;
-	margin: 10px 0px;
 	color: #f7d169;
 	background: #fdf8ec;
 	border: 1px solid #f7d169;
-	border-radius: var(--radius);
 }
 .alert-danger {
-	font-size: 0.8rem;
-	display: block;
-	padding: 10px 20px;
-	margin: 10px 0px;
 	color: #f23;
 	background: #fddbe9;
 	border: 1px solid #f23;
-	border-radius: var(--radius);
 }
 .alert-error {
-	font-size: 0.8rem;
-	display: block;
-	padding: 10px 20px;
-	margin: 10px 0px;
 	color: #fff;
 	background: #f00;
 	border: 1px solid #f00;
-	box-shadow: 0px 0px 0px 3px rgba(255, 0, 0, 0.15);
-	border-radius: var(--radius);
+	box-shadow: 0px 0px 0px 5px rgba(247, 56, 56, 0.3);
+}
+
+@media (prefers-color-scheme: dark) {
+	.alert-default {
+		background: transparent !important;
+	}
 }
 </style>
