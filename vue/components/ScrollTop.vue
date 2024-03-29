35d4aaec8cb9d06382715a28db26fefd549125ea
Atomjoy <atomjoy.official@gmail.com>
2024-03-29 12:47:31 +0100

    Vue3 Scroll to top button
    Vue3 Scroll to top button

---
+++ vue/components/ScrollTop.vue
@@ -0,0 +1,35 @@
+<script>
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: 'smooth' })
+	// window.scrollTo(0,0);
+	// window.scrollIntoView({ behavior: 'smooth' })
+}
+</script>
+<template>
+	<a href="#" @click="scrollToTop">
+		<div class="scrolltop">
+			UP <!-- <font-awesome-icon :icon="['fas', 'chevron-up']" /> -->            
+		</div>
+	</a>
+</template>
+
+<style>
+html {
+	scroll-behavior: smooth;
+}
+
+.scrolltop {
+	color: #fff;
+	background: var(--c3);
+	border-radius: 50%;
+	position: fixed;
+	bottom: 10px;
+	right: 10px;
+	padding: 10px;
+	width: 40px;
+	height: 40px;
+	text-align: center;
+}
+</style>
