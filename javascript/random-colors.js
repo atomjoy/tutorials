4da49148be3fa185ba14338310c7f4bc3b833bfa
Atomjoy <atomjoy.official@gmail.com>
2024-06-04 10:13:40 +0200

    Update random-colors.js Hex, Hsl, changeRootProperty

--- javascript/random-colors.js
+++ javascript/random-colors.js
@@ -1,16 +1,24 @@
-const color = '#' + Math.random().toString(16).substring(2, 6)
-
 function randomColor() {
-	const letters = '0123456789ABCDEF'
-	let color = '#'
-
-	for (let i = 0; i < 6; i++) {
+	var letters = '0123456789ABCDEF'
+	var color = '#'
+	for (var i = 0; i < 6; i++) {
 		color += letters[Math.floor(Math.random() * 16)]
 	}
-
 	return color
+}
+
+function randomColorHex() {
+	return '#' + Math.random().toString(16).substring(2, 6)
 }
 
 function randomColorHsl() {
 	return `hsl(${Math.random() * 360}, 100%, 70%)`
 }
+
+const changeRootColor = () => {
+	let color = randomColor()
+	document.documentElement.style.setProperty('--root-color', color)
+	return color
+}
+
+export { randomColor as default, randomColorHex, randomColorHsl, changeRootColor }
